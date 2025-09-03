ccf0e3e7d23214fbcb1b52181dfb5c8d0b406224
Danilo Galisteu <danilogalisteu@gmail.com>
2025-09-02 22:53:38 -0300

    read first page header and get number of tables (cells on page 0)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+import os
 import sys
+from struct import unpack
 # from dataclasses import dataclass
 
 # import sqlparse - available if you need it!
@@ -11,5 +13,11 @@ if command == ".dbinfo":
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
         print(f"database page size: {page_size}")
+
+        database_file.seek(100, os.SEEK_SET)
+        page_header = database_file.read(8)
+        _, _, num_cells, _, _ = unpack(">BHHHB", page_header)
+        print(f"number of tables: {num_cells}")
+
 else:
     print(f"Invalid command: {command}")
